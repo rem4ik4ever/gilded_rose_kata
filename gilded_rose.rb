85bb17be69de47af7d88f44895a8c6d48ebd15bb
Rem Kim <ursenpai@Rems-MacBook-Air.local>
2020-01-13 17:43:06 -0500

    solution

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,46 +1,33 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+    exception_item_list = ['Aged Brie', 'Backstage passes to a TAFKAL80ETC concert', 'Conjured Mana Cake']
+    is_legendary = item.name == 'Sulfuras, Hand of Ragnaros'
+    if !exception_item_list.include?(item.name)
+      item.quality -= 1 if item.quality > 0 && !is_legendary
+    elsif item.name == 'Conjured Mana Cake'
+      item.quality -= 2 if item.quality > 0 && !is_legendary
     else
       if item.quality < 50
         item.quality += 1
         if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
+          item.quality += 1 if item.quality < 50 && item.sell_in < 11
+          item.quality += 1 if item.sell_in < 6 && item.quality < 50 
         end
       end
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
+    
+    item.sell_in -= 1 if !is_legendary
+    
     if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
+      case item.name
+      when "Aged Brie"
+        item.quality += 1 if item.quality < 50
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality = 0
+      when 'Conjured Mana Cake'
+        item.quality -= 2 if !is_legendary && item.quality > 0
+      else 
+        item.quality -= 1 if !is_legendary && item.quality > 0
       end
     end
   end
